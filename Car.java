e445d5c176fe7a0ea2d1fa1207ff661c9d94d66e
jcobb2020 <33097974+jcobb2020@users.noreply.github.com>
2018-11-03 19:02:12 +0100

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,156 @@
+package agh.cs.lab2;
+import java.util.ArrayList;
+import java.util.List;
+//import com.sun.java.util.jar.pack.Instruction;
+
+public class Car {
+    private MapDirection direction = MapDirection.NORTH;
+    private Position position;
+    private IWorldMap map;
+
+    public String toString() {
+        switch (this.direction) {
+            case SOUTH:
+                return "S";
+            case NORTH:
+                return "N";
+            case WEST:
+                return "W";
+            case EAST:
+                return "E";
+            default:
+                return "XD?";
+        }
+        // return "D";
+        //stara metoda toString
+        //  return (this.position + " " + this.direction);
+    }
+
+    public Car(IWorldMap map) {
+        this.map = map;
+        this.position = new Position(2,2);
+    }
+
+    public Car(IWorldMap map, int x, int y) {
+        this.map = map;
+        this.position = new Position(x, y);
+    }
+
+
+    public static void main(String[] args) {
+        MoveDirection[] directions = new OptionsParser().parse(args);
+        List<HayStack> hayStacks = new ArrayList<>();
+        hayStacks.add(new HayStack(new Position(-4, -4)));
+        hayStacks.add(new HayStack(new Position(7, 7)));
+        hayStacks.add(new HayStack(new Position(3, 3)));
+        hayStacks.add(new HayStack(new Position(2, 0)));
+
+
+        UnboundedMap map = new UnboundedMap(hayStacks);
+//        System.out.println(map.toString());
+
+        map.place(new Car(map));
+        map.place(new Car(map,3,4));
+        map.run(directions);
+
+
+
+    }
+
+    public void move(MoveDirection dir) {
+
+        switch (dir) {
+            case WRONG:
+                return;
+            case LEFT:
+                this.direction = this.direction.previous();
+                return;
+            case RIGHT:
+                this.direction = this.direction.next();
+                return;
+            default:
+                break;
+
+        }
+        int sign = 1;
+        if (dir == MoveDirection.BACKWARD) {
+            sign = -1;
+        }
+        switch (this.direction) {
+            case WEST:
+                sign = sign * -1;
+                break;
+            case SOUTH:
+                sign = sign * -1;
+                break;
+            default:
+                break;
+
+        }
+        Position newPosition;
+        Position vectorX = new Position(sign, 0);
+        Position vectorY = new Position(0, sign);
+        if (this.direction == MapDirection.NORTH || this.direction == MapDirection.SOUTH) {
+            newPosition = this.position.add(vectorY);
+        } else {
+            newPosition = this.position.add(vectorX);
+        }
+        if (this.map.canMoveTo(newPosition)) {   // new chceking move avability
+            this.position = newPosition;
+        }
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+
+}
+
+
+/*case FORWARD:
+                switch (direction) {
+                    case NORTH:
+                        Position newPositionN = new Position(position.x, position.y + 1);
+                        position = newPositionN;
+                        break;
+                    case SOUTH:
+                        Position newPositionS = new Position(position.x, position.y - 1);
+                        position = newPositionS;
+                        break;
+                    case EAST:
+                        Position newPositionE = new Position(position.x + 1, position.y);
+                        position = newPositionE;
+                        break;
+                    case WEST:
+                        Position newPositionW = new Position(position.x - 1, position.y);
+                        position = newPositionW;
+                        break;
+
+                    // default:
+                    // break;
+                }
+                position = checkPosition(position);
+                System.out.println(position);
+                break;
+            case BACKWARD:
+                switch (this.direction) {
+                    case NORTH:
+                        Position newPositionN = new Position(this.position.x, this.position.y - 1);
+                        this.position = newPositionN;
+                        break;
+                    case SOUTH:
+                        Position newPositionS = new Position(this.position.x, this.position.y + 1);
+                        this.position = newPositionS;
+                        break;
+                    case EAST:
+                        Position newPositionE = new Position(this.position.x - 1, this.position.y);
+                        this.position = newPositionE;
+                        break;
+                    case WEST:
+                        Position newPositionW = new Position(this.position.x + 1, this.position.y);
+                        this.position = newPositionW;
+                        break;
+                }
+                break;
+                */
---
+++ UnboundedMap.java
@@ -0,0 +1,105 @@
+package agh.cs.lab2;
+
+import org.omg.PortableInterceptor.ObjectReferenceFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UnboundedMap extends AbstractWorldMap{
+
+    List<HayStack> hayStacks = new ArrayList<>();
+   // List<Car> cars = new ArrayList<>();
+
+    public UnboundedMap(List<HayStack> hayS) {
+        this.hayStacks = hayS;
+    }
+
+   @Override
+    public boolean isOccupied(Position position) {
+//        for (int i = 0; i < cars.size(); i++) {
+//            Car currentCar = cars.get(i);
+//            if (currentCar.getPosition().equals(position)) {
+//
+//                return true;
+//            }
+//        }
+        if (super.isOccupied(position)){
+            return true;
+       }
+        for (int i = 0; i < hayStacks.size(); i++) {
+            HayStack currentHS = hayStacks.get(i);
+            if (currentHS.getPosition().equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    public boolean canMoveTo(Position position) {
+//        if (this.isOccupied(position)) {
+//            return false;
+//        }
+//        return true;
+//    }
+//    @Override
+//    public boolean place(Car car) {
+//        if (this.canMoveTo(car.getPosition())) {
+//            cars.add(car);
+//            return true;
+//        }
+//        System.out.println("placeError");
+//        return false;
+//    }
+
+//    public void run(MoveDirection[] directions) {
+//
+//        int movesNumber = directions.length;
+//        int carNum=this.cars.size();
+//        System.out.println("carNum =" + carNum);
+//        int currentCarNum;
+//        Car currentCar;
+//        for(int i=0; i<movesNumber; i++){
+//            currentCarNum=i%carNum;
+//            currentCar = cars.get(currentCarNum);
+//            currentCar.move(directions[i]);
+//            System.out.println("currentCar=" + currentCarNum);
+//            System.out.println(currentCar.getPosition().x);
+//            System.out.println(currentCar.getPosition().y);
+//            System.out.println(currentCar.toString());
+//            System.out.println(this.toString());
+//        }
+//        System.out.println(this.toString());
+//    }
+
+    public Object objectAt(Position pos){
+        for(Car car : this.cars){
+            if (car.getPosition().equals(pos)){
+                return car;
+            }
+        }
+        for(HayStack hS : this.hayStacks){
+            if (hS.getPosition().equals(pos)){
+                return hS;
+            }
+        }
+        return null;
+    }
+
+    public String toString (){
+        Position lowerLeft = new Position(0,0);
+        Position upperRight = new Position(0,0);
+        for(Car auto : this.cars){
+            lowerLeft=lowerLeft.lowerLef(auto.getPosition());
+            upperRight=upperRight.upperRight(auto.getPosition());
+        }
+        for(HayStack hs : this.hayStacks){
+            lowerLeft=lowerLeft.lowerLef(hs.getPosition());
+            upperRight=upperRight.upperRight(hs.getPosition());
+        }
+
+        MapVisualizer visualizer = new MapVisualizer(this);
+        return visualizer.draw(lowerLeft, upperRight);
+    }
+}
+
