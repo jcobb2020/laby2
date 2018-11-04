4a5920252795ef9dc9c7b5b83da8a7418b4bc65a
JCobb2020 <jakubgol2020@gmail.com>
2018-11-05 00:44:15 +0100

    Updated Abstract World Object Class

--- src/main/agh/cs/lab2/AbstractWorldObject.java
+++ src/main/agh/cs/lab2/AbstractWorldObject.java
@@ -2,10 +2,6 @@ package agh.cs.lab2;
 
 abstract class AbstractWorldObject {
     protected Position position;
-
-//    public AbstractWorldObject(){
-//        this.position=new Position(0,0);
-//    }
     public Position getPosition(){
         return this.position;
     }
